6f52e3b00874c824210a3d1ad7e1a610b5e85a3e
audiobalance <41115082+audiobalance@users.noreply.github.com>
2018-07-11 18:33:47 +0300

    Add files via upload

--- code/config.js
+++ code/config.js
@@ -5,8 +5,8 @@
 			"impetus": "true", //"false"
 			"containerSelector": ".pro360",
 			"autoplay": {"interval": 100, "bounce": false},//frame timeout
-			"folder":"https://audiobalance.sirv.com/360/002/500/",
-			"zoomfolder":"https://audiobalance.sirv.com/360/002/2000/",
+			"folder":"500/",
+			"zoomfolder":"2000/",
 			"file": "{XXX}.jpg", // {-XXX}.jpg for order inversion
 			//"invertScroll": true, // uncomment for inverting scroll direction
 			"type": "row",
